4df83db93f87d3acccc34306344832b7066e9fbb
Jonathan Costa <jonathanfernandes638@gmail.com>
2023-09-18 14:34:05 -0300

    fix: nome

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,7 @@ function Header (){
         <div className="mb-8">
             <header className="bg-purple-800 flex text-center justify-between font-bold p-2 text-white">
                 <h1 className="font-lobster p-3 border border-white rounded-full"> Lista de Contatos</h1>
-                <h2 className="grid items-center">Estágio BOX³</h2>
+                <h2 className="grid items-center">Estagiário:  Jonathan Fernandes</h2>
             </header>
         </div>
     )
